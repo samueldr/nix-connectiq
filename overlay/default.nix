f83b659d351e6793eb5ce7c58c11da981c4ab836
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-08-04 20:52:29 -0400

    Also build the beta SDK

--- overlay/default.nix
+++ overlay/default.nix
@@ -10,5 +10,13 @@ self: super:
       revision = "2019-06-12-77ed6f47e";
       sha256 = "0pc1vgfm3gdw0bc06602k78q658r4x9144nvaidqnagn7iaq9b86";
     };
+    beta_sdk = self.callPackage ./sdk {
+      libpng = super.libpng12;
+      libjpeg = self.libjpeg8;
+      sdkver = "3.1.0.Beta3";
+      beta = true;
+      revision = "2019-06-14-bf21fff97";
+      sha256 = "1ln4w99d8j4c4l0k0f564a0yys4bnfg5kj88ihbxrps9xkx79ckj";
+    };
   };
 }
