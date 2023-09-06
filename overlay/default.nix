614af639163f1b789c2a437e0ec3dc2da2d9fb31
Hugo Herter <git@hugoherter.com>
2023-09-07 00:26:27 +0200

    Fix: Update SDK and Nix dependencies

--- overlay/default.nix
+++ overlay/default.nix
@@ -6,9 +6,9 @@ self: super:
     sdk = self.callPackage ./sdk {
       libpng = super.libpng12;
       libjpeg = self.libjpeg8;
-      sdkver = "3.0.12";
-      revision = "2019-06-12-77ed6f47e";
-      sha256 = "0pc1vgfm3gdw0bc06602k78q658r4x9144nvaidqnagn7iaq9b86";
+      sdkver = "6.3.0";
+      revision = "2023-08-29-fc81ed416";
+      sha256 = "sha256-A4rU0B9MYz5XxRLWEhqjQPyngFgH1z81O1FlLXppkkk=";
     };
 
     beta_sdk = self.callPackage ./sdk {
